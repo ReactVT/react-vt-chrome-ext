7db57fe70dec5101fbc9292a14a14634499bba09
icrustandi <icrustandi@gmail.com>
2017-08-12 12:50:06 -0700

    Added padding, selection user flow

--- client/components/TestLocation.js
+++ client/components/TestLocation.js
@@ -1,36 +1,106 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Dropdown, Input } from 'semantic-ui-react';
 
 class TestLocation extends Component {
   constructor(props) {
     super(props);
-    // default to node
-    this.locationSelect = 'node';
+    // default to nothing
+    this.currentSelector = '';
+    this.currentSelectorName = '';
+    this.currentModifier = '';
   }
   handleSubmitEventForAction(event) {
       event.preventDefault();
-      this.props.saveTestProperty('selector', this.locationSelect);
-      this.props.setTestLocation(this.props.compAddress);
+      let arrayIndex = document.getElementById('selectorIndexInput').value;
+      if(this.currentModifier === 'index') {
+        arrayIndex = '[' + JSON.stringify(arrayIndex) + ']'
+        this.props.saveTestProperty('modifier', arrayIndex);
+      }
+      if (this.currentSelector === 'node') this.props.setTestLocation(this.props.compAddress);
+      console.log('after test submit - STATE', this.props.stateIsNowProp);
       this.props.renderTest2();
-      // this.props.setActionLocation(this.props.compAddress);
-      // this.props.saveAssertion(this.props.stateIsNowProp.action);
     };
 
-    // handleEventDropdown(event) {
-    //   this.props.saveActionProperty('event', event.target.value);
-    //   console.log('handled event dropdown', event.target.value)
-    // }
+    handleSelectorDropdown(event, value) {
+      this.currentSelector = value;
+      this.props.saveTestProperty('selector', value);
+      console.log('handled selector dropdown', value)
+    }
+
+    handleSelectorNameDropdown(event, value) {
+      this.currentSelectorName = value;
+      this.props.saveTestProperty('selectorName', value);
+      console.log('handled selectorName dropdown', value)
+      
+    }
+
+    handleModifierDropdown(event, value) {
+      this.currentModifier = value;
+      if(this.currentModifier === '.length') {
+        this.props.saveTestProperty('modifier', value)
+      }
+      console.log('handled modifier dropdown', value)
+      
+    }
 
     handleBack() {
       this.props.renderEditMode();
     }
   
   render () {
-    let locationDetails;
-    // console.log('loc detiall', document.getElementById('locationDropdown'))
-    if (this.locationSelect === 'node') locationDetails = (<input value = {this.props.compAddress} disabled />);
-    else locationDetails = (<input type="text"/>);
+    let selectorNameRender;
+    let modifierRender;
+    let indexRender;
+    const selectorName = [];
+    const selector = [
+      { key: 1, text: 'Node', value: 'node' },
+      { key: 2, text: 'Component', value: 'component' },
+      { key: 3, text: 'ID', value: 'id' },
+      { key: 4, text: 'Class', value: 'class' },
+      { key: 5, text: 'Tag', value: 'tag' }
+    ];
+    const modifier = [
+      { key: 1, text: 'Length', value: '.length' },
+      { key: 2, text: 'Index', value: 'index' }
+    ]
+
+    // Selector and Selector name logic
+    if (this.currentSelector === 'node') {
+      selectorName.push({ key: 1, text: this.props.compAddress, value: this.props.compAddress });
+      selectorNameRender = (<Input placeholder='Click on a node' value = {this.props.compAddress} disabled />);
+    } else if (this.currentSelector !== '') {
+      if (this.currentSelector === 'component'){
+        let components = this.props.stateIsNowProp.nodeStore.node;
+        Object.keys(components).forEach((compName, i)=> {
+          selectorName.push({ key: i, text: compName, value: compName });
+        });
+      } else if (this.currentSelector === 'id') {
+        let id = this.props.stateIsNowProp.nodeStore.id;
+        selectorName.push({ key: 1, text: id, value: id });
+      } else if (this.currentSelector === 'class') {
+        let classes = this.props.stateIsNowProp.nodeStore.class;
+        Object.keys(classes).forEach((className, i)=> {
+          selectorName.push({ key: i, text: className, value: className });
+        });
+      } else if (this.currentSelector === 'tag') {
+        let tags = this.props.stateIsNowProp.nodeStore.tag;
+        Object.keys(tags).forEach((tagName, i)=> {
+          selectorName.push({ key: i, text: tagName, value: tagName });
+        });
+      }
+      selectorNameRender=(<Dropdown selection options={selectorName} id="selectorNameDropdown" defaultValue = '1' onChange={(e, {value})=>this.handleSelectorNameDropdown(e, value)} />);
+    }
+
+    // Modifier logic
+    if (this.currentSelector !== 'node' && this.currentSelector !== 'id') {
+      modifierRender = (<Dropdown selection options={modifier} id="modifierDropdown" defaultValue = '1' onChange={(e, {value})=>this.handleModifierDropdown(e, value)} />);
+    }
+
+    if (this.currentModifier === 'index') {
+      indexRender = (<Input className="indexInput" id="selectorIndexInput" type = "number"/>);
+    }
+      
     return (
 
       <form onSubmit={(event)=>{
@@ -41,10 +111,11 @@ class TestLocation extends Component {
 
         <div className="form-group">
           <label>Location <span style={ {color: "#ffaaaa"} }>*</span></label>
-          <select id="locationDropdown" onChange={(e)=>this.handleEventDropdown(e)}>
-            <option value="node">Node</option>
-          </select>
-          { locationDetails }
+          <Dropdown selection options={selector} placeholder="Selector" id="selectorDropdown" onChange={(e, {value})=>this.handleSelectorDropdown(e, value)} />
+          <br />
+          { selectorNameRender }
+          <br />
+          { modifierRender } { indexRender }
         </div>
         <Button primary onClick={()=>this.handleBack()} className="btn btn-primary">Back</Button>
         <Button primary type="submit" className="btn btn-primary">Save</Button>
