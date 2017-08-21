cbefddd052db9ebc33be6f8ee4d3c0dd84096482
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-20 17:28:46 -0700

    Fixed issue with local assert status on initial load not resetting.

--- client/components/AddAssertionsForAction.js
+++ client/components/AddAssertionsForAction.js
@@ -48,7 +48,7 @@ class AddAssertionsForAction extends Component {
       { key: 1, text: 'Click', value: 'click' },
       { key: 2, text: 'Double Click', value: 'dblclick' },
       { key: 3, text: 'Right Click', value: 'contextmenu' },
-      { key: 4, text: 'Enter', value: 'onEnter' }      
+      { key: 4, text: 'Enter', value: 'keyup' }      
     ];
     if (this.props.compName) this.error = '';
     return (
