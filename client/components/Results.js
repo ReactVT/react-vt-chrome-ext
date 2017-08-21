dca7f6a1ea4f2ace110d01306d9c7546a08706a7
icrustandi <icrustandi@gmail.com>
2017-08-21 12:26:40 -0700

    styling: fixed assertion list transitions, changed action icon color

--- client/components/Results.js
+++ client/components/Results.js
@@ -36,7 +36,7 @@ class Results extends Component {
       if (currentResult.result === true && currentResult.comparator === 'action') {
         this.results.push(
           <List.Item>
-            <List.Icon name="arrow right" color='green' />
+            <List.Icon name="arrow right" color='blue' />
          ID{currentResult.assertID} Action from {currentResult.assertionBlock} was performed
 </List.Item>);
         // if test passes
